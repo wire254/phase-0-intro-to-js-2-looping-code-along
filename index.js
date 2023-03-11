b4b9f1581679dfe61e21413a99e8bd6ec5512f14
Ulises <ulises.argueta95@gmail.com>
2023-03-10 18:15:40 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const messageNames = [];
+
+function writeCards(names, value) {
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames
+}
+
+function countDown(number) {
+    let value = number
+    while (value >= 0) {
+        console.log(value--);
+    }
+    return number;
+}
